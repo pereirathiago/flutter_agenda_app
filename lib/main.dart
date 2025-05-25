50970d7cb95e9ee5ac3dad86e9d7a09e21d14f64
Thiago Pereira <pereira.tp.thiago@gmail.com>
2025-05-25 16:46:53 -0300

    configuracao inicial firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_agenda_app/app.dart';
+import 'package:flutter_agenda_app/firebase_options.dart';
 import 'package:flutter_agenda_app/repositories/appointments_repository_memory.dart';
 import 'package:flutter_agenda_app/repositories/invitation_repository_memory.dart';
 import 'package:flutter_agenda_app/repositories/location_repository.dart';
 import 'package:flutter_agenda_app/repositories/location_repository_sqlite.dart';
 import 'package:flutter_agenda_app/repositories/user_repository.dart';
 import 'package:flutter_agenda_app/repositories/user_repository_sqlite.dart';
+import 'package:flutter_agenda_app/services/auth_service.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(
     MultiProvider(
       providers: [
+        ChangeNotifierProvider(create: (context) => AuthService()),
         ChangeNotifierProvider<UserRepository>(
           create: (context) => UserRepositorySqlite(),
         ),
